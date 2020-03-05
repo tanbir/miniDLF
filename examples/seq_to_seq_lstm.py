df80d21ffa53cbde8e0de3a747543826904c2456
Tanbir Ahmed <tanbir@gmail.com>
2020-03-05 08:27:47 -0500

    Add files via upload

--- examples/seq_to_seq_lstm.py
+++ examples/seq_to_seq_lstm.py
@@ -4,16 +4,13 @@ from miniDLF.layers import LSTM, Activation
 from miniDLF.optimizers import Adam
 from miniDLF.datasets import TEXT2SEQ
 
-
-
-d = TEXT2SEQ('./data/TEXT/basic_rnn.txt', 50)
+d = TEXT2SEQ('./data/TEXT/basic_rnn_from_wiki.txt', 60)
 
 m = Sequential() 
-m.add(LSTM(512, input_shape=d.input_shape))
+m.add(LSTM(256, input_shape=d.input_shape))
+
 m.add(Activation('softmax'))
 m.compile(loss='cce', optimizer=Adam())
 m.summary()
 
-
-
-m.fit(dataset=d, epochs=1000, minibatch_size = 50, accuracy_threshold=0.96, early_stop_after = 30)
+m.fit(dataset=d, epochs=1000, minibatch_size = 25, accuracy_threshold=0.95, early_stop_after = 30)
